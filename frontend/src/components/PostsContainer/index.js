ccbb24318599db532c4be185816eacf9a1d01a29
Ji <jikyung@gmail.com>
2021-07-20 12:46:02 -0400

    initial commit

--- frontend/src/components/PostsContainer/index.js
+++ frontend/src/components/PostsContainer/index.js
@@ -1,7 +1,8 @@
 // import hooks from 'react-redux'
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
-import styles from './PostsContainer.css'
+import './PostsContainer.css'
+import { compareAsc, format } from 'date-fns' // date format ????
 
 // import the thunk creator
 import { getPosts } from '../../store/posts';
@@ -20,7 +21,11 @@ const PostsContainer = () => {
         posts.reverse().map(post => (
             <div className='post-container'>
                 <div className='post-title'>{post.title}</div>
+                {/* <div className='post-author'>{post.userId.User.id.username}</div> */}
+                {/* <div className='post-author'>{post.User.username}</div> */}
                 {/* <div className='post-author'>{post.id}</div> */}
+                <div className="post-date">{post.createdAt}</div>
+                {/* <div className="post-date">{post.createdAt.toLocaleDateString()}</div> */}
                 <div className='post-content'>{post.content}</div>
             </div>
         ))
