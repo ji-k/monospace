0f605f730cbae9abd88e9c2bcc83ca564d024bbf
Ji <jikyung@gmail.com>
2021-07-21 12:32:50 -0400

    implement optional chaining (?.) operator

--- frontend/src/components/PostPage/index.js
+++ frontend/src/components/PostPage/index.js
@@ -23,9 +23,9 @@ const PostPage = () => {
 
     return (
         <div className="single-post">
-            <div className="single-post-title">{postPage.title}</div>
-            <div className="single-post-date">{postPage.createdAt}</div>
-            <div className="single-post-content">{postPage.content}</div>
+            <div className="single-post-title">{postPage?.title}</div>
+            <div className="single-post-date">{postPage?.createdAt}</div>
+            <div className="single-post-content">{postPage?.content}</div>
         </div>
     )
 }
