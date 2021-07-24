327b4af89e1720627a1556f269418cebe7e45038
Ji <jikyung@gmail.com>
2021-07-24 19:46:49 -0400

    style nav icon

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -31,7 +31,7 @@ function ProfileButton({ user }) {
 
   return (
     <>
-      <button onClick={openMenu}>
+      <button className="fa-icon" onClick={openMenu}>
         <i className="fas fa-user-circle" />
       </button>
       {showMenu && (
